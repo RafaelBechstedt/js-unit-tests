5db510c6a951debe93865e4e1624300b6e8e9ef7
Rafael <bechstedt@hotmail.com>
2022-06-27 20:35:53 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,8 +37,14 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se o retorno da função é um array.
     expect(Array.isArray(productDetails('a', 'b'))).toBe(true)
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('a', 'b').length).toBe(2)
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof Object.keys(productDetails('a', 'b')[0])).toBe('object')
+    expect(typeof Object.keys(productDetails('a', 'b')[1])).toBe('object')
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('a', 'b')[0]).not.toEqual(productDetails('a', 'b')[1])
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('a', 'b')[0].details.productId.endsWith('123')).toBe(true)
+    expect(productDetails('a', 'b')[1].details.productId.endsWith('123')).toBe(true)
   });
 });
